26aa26a9f5f2c3de5e41efdb23ff7891278242ca
Francis Eshun <eshunfrancisboateng@gmail.com>
2022-05-31 16:42:37 +0000

    init

---
+++ src/config.js
@@ -0,0 +1,186 @@
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const { MODE } = require(path.join(basePath, "src/blendMode.js"));
+
+const buildDir = path.join(basePath, "/build");
+const layersDir = path.join(basePath, "/layers");
+
+/*********************
+ * General Generator Options
+ ***********************/
+
+const description =
+  "This is the description of your NFT project, remember to replace this";
+const baseUri = "ipfs://NewUriToReplace";
+
+const outputJPEG = false; // if false, the generator outputs png's
+
+/**
+ * Set your tokenID index start number.
+ * ⚠️ Be sure it matches your smart contract!
+ */
+const startIndex = 0;
+
+const format = {
+  width: 1024,
+  height: 1024,
+  smoothing: true, // set to false when up-scaling pixel art.
+};
+
+const background = {
+  generate: true,
+  brightness: "100%",
+};
+
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 64,
+    namePrefix: "NZMX Club", // Use to add a name to Metadata `name:`
+    layersOrder: [
+      { name: "Background" },
+      { name: "Skin" },
+      { name: "Eyes" },
+      { name: "Clothes" },
+      { name: "Head Accessory" },
+      { name: "Bling" },
+    ],
+  },
+  
+];
+
+/**
+ * Set to true for when using multiple layersOrder configuration
+ * and you would like to shuffle all the artwork together
+ */
+const shuffleLayerConfigurations = false;
+
+const debugLogs = true;
+
+/*********************
+ * Advanced Generator Options
+ ***********************/
+
+// if you use an empty/transparent file, set the name here.
+const emptyLayerName = "NONE";
+
+/**
+ * Incompatible items can be added to this object by a files cleanName
+ * This works in layer order, meaning, you need to define the layer that comes
+ * first as the Key, and the incompatible items that _may_ come after.
+ * The current version requires all layers to have unique names, or you may
+ * accidentally set incompatibilities for the _wrong_ item.
+ */
+const incompatible = {
+  //   Red: ["Dark Long"],
+  //   // directory incompatible with directory example
+  //   White: ["rare-Pink-Pompadour"],
+};
+
+/**
+ * Require combinations of files when constructing DNA, this bypasses the
+ * randomization and weights.
+ *
+ * The layer order matters here, the key (left side) is an item within
+ * the layer that comes first in the stack.
+ * the items in the array are "required" items that should be pulled from folders
+ * further in the stack
+ */
+const forcedCombinations = {
+  // floral: ["MetallicShades", "Golden Sakura"],
+};
+
+/**
+ * In the event that a filename cannot be the trait value name, for example when
+ * multiple items should have the same value, specify
+ * clean-filename: trait-value override pairs. Wrap filenames with spaces in quotes.
+ */
+const traitValueOverrides = {
+  Helmet: "Space Helmet",
+  "gold chain": "GOLDEN NECKLACE",
+};
+
+const extraMetadata = {};
+
+const extraAttributes = () => [
+  // Optionally, if you need to overwrite one of your layers attributes.
+  // You can include the same name as the layer, here, and it will overwrite
+  //
+  // {
+  // trait_type: "Bottom lid",
+  //   value: ` Bottom lid # ${Math.random() * 100}`,
+  // },
+  // {
+  //   display_type: "boost_number",
+  //   trait_type: "Aqua Power",
+  //   value: Math.random() * 100,
+  // },
+  // {
+  //   display_type: "boost_number",
+  //   trait_type: "Health",
+  //   value: Math.random() * 100,
+  // },
+  // {
+  //   display_type: "boost_number",
+  //   trait_type: "Mana",
+  //   value: Math.floor(Math.random() * 100),
+  // },
+];
+
+// Outputs an Keccack256 hash for the image. Required for provenance hash
+const hashImages = true;
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+/**
+ * Set to true to always use the root folder as trait_type
+ * Set to false to use weighted parent folders as trait_type
+ * Default is true.
+ */
+const useRootTraitType = true;
+
+const preview = {
+  thumbPerRow: 8,
+  thumbWidth: 1000,
+  imageRatio: 1,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 64,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 1,
+  quality: 200,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  background,
+  baseUri,
+  buildDir,
+  debugLogs,
+  description,
+  emptyLayerName,
+  extraAttributes,
+  extraMetadata,
+  forcedCombinations,
+  format,
+  hashImages,
+  incompatible,
+  layerConfigurations,
+  layersDir,
+  outputJPEG,
+  preview,
+  preview_gif,
+  rarityDelimiter,
+  shuffleLayerConfigurations,
+  startIndex,
+  traitValueOverrides,
+  uniqueDnaTorrance,
+  useRootTraitType,
+};
